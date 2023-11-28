38e2e726ef51a1a4369874149d93e0fdecdffb90
johnvmp275 <marquesjoaov1@gmail.com>
2023-11-28 16:56:22 -0300

    correcao e slides dinamicos

--- js/modules/fetchProducts.js
+++ js/modules/fetchProducts.js
@@ -1,134 +1,133 @@
 import activeItemsProducts from "./favoriteProduct.js";
 
 export default function fetchProducts() {
+  // Função para inicializar o Swiper
 
-    // Função para inicializar o Swiper
+  function initializeSwiper() {
+    //=====swiper de categorias====
+    new Swiper(".swiper-categorias", {
+      slidesPerView: 5,
+      spaceBetween: 40,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        720: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+      },
+    });
+  }
 
-    function initializeSwiper() {
+  function initializeProdutoPrincipal() {
+    //=====swiper de produtos====
 
-        //=====swiper de categorias====
-        var swiper = new Swiper(".swiper-categorias", {
-            slidesPerView: 5,
-            spaceBetween: 40,
-            loop: true,
-            autoplay: {
-                delay: 3000,
-                disableOnInteraction: false,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            }, breakpoints: {
-                0: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                720: {
-                    slidesPerView: 5,
-                    spaceBetween: 40,
-                },
-            },
-        });
+    new Swiper(".swiper-products-principal", {
+      slidesPerView: 5,
+      spaceBetween: 40,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        720: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+      },
+    });
+  }
+  function initializeProdutoSecundario() {
+    //=====swiper de produtos====
 
+    new Swiper(".swiper-products-secundario", {
+      slidesPerView: 5,
+      spaceBetween: 40,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        720: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+      },
+    });
+  }
 
-    }
+  function initializePrincipalBanner() {
+    new Swiper(".swiper-banner", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
 
-    function initializeProdutoPrincipal() {
-        //=====swiper de produtos====
+  function initializeBannerSecundario() {
+    new Swiper(".swiper-banner-secundario", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
 
-        var swiper = new Swiper(".swiper-products", {
-            slidesPerView: 5,
-            spaceBetween: 40,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            }, breakpoints: {
-                0: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                720: {
-                    slidesPerView: 5,
-                    spaceBetween: 40,
-                },
-            },
-        });
-    }
-    function initializeProdutoSecundario() {
-        //=====swiper de produtos====
+  async function fetchProductsPrimario() {
+    try {
+      const response = await fetch("./json/slidePrimario.json");
+      const data = await response.json();
 
-        var swiper = new Swiper(".swiper-products-secundario", {
-            slidesPerView: 5,
-            spaceBetween: 40,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            }, breakpoints: {
-                0: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                720: {
-                    slidesPerView: 5,
-                    spaceBetween: 40,
-                },
-            },
-        });
-    }
+      const swiperWrapper = document.querySelector(".swiper-products-slide");
 
-    function initializePrincipalBanner() {
-        var swiper = new Swiper(".swiper-banner", {
-            loop: true,
-            autoplay: {
-                delay: 3000,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-    }
-
-    function initializeBannerSecundario() {
-        var swiper = new Swiper(".swiper-banner-secundario", {
-            loop: true,
-            autoplay: {
-                delay: 3000,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-    }
-    
-    async function fetchProductsPrimario() {
-        try {
-            const response = await fetch('./json/slidePrimario.json');
-            const data = await response.json();
-
-            const swiperWrapper = document.querySelector('.swiper-products-slide');
-
-            data.forEach(product => {
-                let newSlide = document.createElement('div');
-                newSlide.classList.add('swiper-slide', 'slide-products');
-                newSlide.innerHTML = `
+      data.forEach((product) => {
+        let newSlide = document.createElement("div");
+        newSlide.classList.add("swiper-slide", "slide-products");
+        newSlide.innerHTML = `
                 <div class="product-favorite">
                         <button class="button-favorite">
                             <img src="./img/favorite.png" alt="favorite">
@@ -143,27 +142,29 @@ export default function fetchProducts() {
                         <a class="product-link">Comprar</a>
                     </div>
                 `;
-                swiperWrapper.appendChild(newSlide);
-            });
+        swiperWrapper.appendChild(newSlide);
+      });
 
-            // Inicialize o Swiper após adicionar os slides
-            initializeProdutoPrincipal();
-        } catch (error) {
-            console.error('Houve um erro ao buscar os produtos:', error);
-        }
+      // Inicialize o Swiper após adicionar os slides
+      initializeProdutoPrincipal();
+    } catch (error) {
+      console.error("Houve um erro ao buscar os produtos:", error);
     }
+  }
 
-    async function fetchProductsSecundario() {
-        try {
-            const response = await fetch('./json/slideSecundario.json');
-            const data = await response.json();
+  async function fetchProductsSecundario() {
+    try {
+      const response = await fetch("./json/slideSecundario.json");
+      const data = await response.json();
 
-            const swiperWrapper = document.querySelector('.swiper-products-slideSecundario');
+      const swiperWrapper = document.querySelector(
+        ".swiper-products-slideSecundario"
+      );
 
-            data.forEach(product => {
-                let newSlide = document.createElement('div');
-                newSlide.classList.add('swiper-slide', 'slide-products');
-                newSlide.innerHTML = `
+      data.forEach((product) => {
+        let newSlide = document.createElement("div");
+        newSlide.classList.add("swiper-slide", "slide-products");
+        newSlide.innerHTML = `
                 <div class="product-favorite">
                         <button class="button-favorite">
                             <img src="./img/favorite.png" alt="favorite">
@@ -178,88 +179,86 @@ export default function fetchProducts() {
                         <a class="product-link">Comprar</a>
                     </div>
                 `;
-                swiperWrapper.appendChild(newSlide);
-            });
-            initializeProdutoSecundario()
-            activeItemsProducts();
-        } catch (error) {
-            console.error('Houve um erro ao buscar os produtos:', error);
-        }
+        swiperWrapper.appendChild(newSlide);
+      });
+      initializeProdutoSecundario();
+      activeItemsProducts();
+    } catch (error) {
+      console.error("Houve um erro ao buscar os produtos:", error);
     }
+  }
 
-    async function fetchCategories() {
-        try {
-            const response = await fetch('./json/slideCategories.json');
-            const data = await response.json();
+  async function fetchCategories() {
+    try {
+      const response = await fetch("./json/slideCategories.json");
+      const data = await response.json();
 
-            const swiperWrapper = document.querySelector('.swiper-categories-slide');
+      const swiperWrapper = document.querySelector(".swiper-categories-slide");
 
-            data.forEach(product => {
-                let newSlide = document.createElement('div');
-                newSlide.classList.add('swiper-slide', 'slide-categorias');
-                newSlide.innerHTML = `
+      data.forEach((product) => {
+        let newSlide = document.createElement("div");
+        newSlide.classList.add("swiper-slide", "slide-categorias");
+        newSlide.innerHTML = `
                 <div class="product-image">
                             <img src="./img/${product.image}" alt="${product.title}" srcset="">
                         </div>
                         <strong>${product.title}</strong>
                 `;
-                swiperWrapper.appendChild(newSlide);
-            });
+        swiperWrapper.appendChild(newSlide);
+      });
 
-            // Inicialize o Swiper após adicionar os slides
-            initializeSwiper();
-        } catch (error) {
-            console.error('Houve um erro ao buscar os produtos:', error);
-        }
+      // Inicialize o Swiper após adicionar os slides
+      initializeSwiper();
+    } catch (error) {
+      console.error("Houve um erro ao buscar os produtos:", error);
     }
+  }
 
-    async function fetchBannerPrincipal() {
-        try {
-            const response = await fetch('./json/bannerPrincipal.json');
-            const data = await response.json();
+  async function fetchBannerPrincipal() {
+    try {
+      const response = await fetch("./json/bannerPrincipal.json");
+      const data = await response.json();
 
-            const swiperWrapper = document.querySelector('.swiper-banner-principal');
+      const swiperWrapper = document.querySelector(".swiper-banner-principal");
 
-            data.forEach(product => {
-                let newSlide = document.createElement('div');
-                newSlide.classList.add('swiper-slide');
-                newSlide.innerHTML = `
+      data.forEach((product) => {
+        let newSlide = document.createElement("div");
+        newSlide.classList.add("swiper-slide");
+        newSlide.innerHTML = `
                         <img src="./img/${product.image}" alt="${product.title}">
                 `;
-                swiperWrapper.appendChild(newSlide);
-
-            });
-            initializePrincipalBanner()
-        } catch (error) {
-            console.error('Houve um erro ao buscar os produtos:', error);
-        }
+        swiperWrapper.appendChild(newSlide);
+      });
+      initializePrincipalBanner();
+    } catch (error) {
+      console.error("Houve um erro ao buscar os produtos:", error);
     }
+  }
 
-    async function fetchBannerSecundario() {
-        try {
-            const response = await fetch('./json/bannerSecundario.json');
-            const data = await response.json();
+  async function fetchBannerSecundario() {
+    try {
+      const response = await fetch("./json/bannerSecundario.json");
+      const data = await response.json();
+      const swiperWrapper = document.querySelector(".swiper-banner-secundario");
 
-            const swiperWrapper = document.querySelector('.swiper-banner-secundario');
-
-            data.forEach(product => {
-                let newSlide = document.createElement('div');
-                newSlide.classList.add('swiper-slide');
-                newSlide.innerHTML = `
+      data.forEach((product) => {
+        let newSlide = document.createElement("div");
+        newSlide.classList.add("swiper-slide");
+        newSlide.innerHTML = `
                         <img src="./img/${product.image}" alt="${product.title}">
                 `;
-                swiperWrapper.appendChild(newSlide);
-            });
-            initializeSwiperBanner() 
-        } catch (error) {
-            console.error('Houve um erro ao buscar os produtos:', error);
-        }
+        swiperWrapper.appendChild(newSlide);
+      });
+      initializeBannerSecundario();
+    } catch (error) {
+      console.error("Houve um erro ao buscar os produtos:", error);
     }
+  }
 
-    // Adicione um listener para quando o DOM estiver pronto
-    document.addEventListener('DOMContentLoaded', fetchProductsPrimario);
-    document.addEventListener('DOMContentLoaded', fetchProductsSecundario);
-    document.addEventListener('DOMContentLoaded', fetchCategories);
-    document.addEventListener('DOMContentLoaded', fetchBannerPrincipal);
-    document.addEventListener('DOMContentLoaded', fetchBannerSecundario);
+  // Adicione um listener para quando o DOM estiver pronto
+  document.addEventListener("DOMContentLoaded", fetchProductsPrimario);
+  document.addEventListener("DOMContentLoaded", fetchProductsSecundario);
+  document.addEventListener("DOMContentLoaded", fetchCategories);
+  document.addEventListener("DOMContentLoaded", fetchBannerPrincipal);
+  document.addEventListener("DOMContentLoaded", fetchBannerSecundario);
 }
